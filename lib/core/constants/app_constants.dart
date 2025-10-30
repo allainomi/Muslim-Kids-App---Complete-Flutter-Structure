930dc2a76312f0ccc2f35950ba08e0ff32f69663
allainomi <your-email@example.com>
2025-10-30 11:19:11 +0500

    Added new feature: [feature name]

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,5 @@
+﻿class AppConstants {
+  static const String appName = 'Muslim Kids';
+  static const String appVersion = '1.0.0';
+  static const String developerName = 'Hafiz Mufti Muhammad Shuaib Khan Alai';
+}
